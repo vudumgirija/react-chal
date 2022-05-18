7885334925a4a36d4a80a02e96505400154ec2c5
vudumgirija <103623179+vudumgirija@users.noreply.github.com>
2022-05-18 13:19:29 +0530

    Add files via upload

---
+++ Posts.js
@@ -0,0 +1,5 @@
+export default function Posts(){
+    return(
+        <h1>This is posts page</h1>
+    )
+}
---
+++ Signup.js
@@ -0,0 +1,6 @@
+import React from'react'
+export default function Signup(){
+    return(
+        <h1>This is signup page</h1>
+    )
+}
